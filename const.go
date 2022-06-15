14ecb684d93385dc9ee0e6f952729dc19ace59e0
john-80 <johnchadjohn@outlook.com>
2022-06-15 10:57:35 +0800

    main

--- const.go
+++ const.go
@@ -11,6 +11,7 @@ const (
 	DebugLevel
 )
 
+
 type PREFIX string
 
 const (
--- log.go
+++ log.go
@@ -1,6 +1,9 @@
 package logf
 
-import "os"
+import (
+	"os"
+	"path"
+)
 
 type LogFile struct {
 	fileFD   *os.File
@@ -12,4 +15,18 @@ type LogFile struct {
 	prefix   PREFIX
 	mode     MODE
 	maxSize  int64
+}
+
+func NewLogFile(fileName string) *LogFile {
+	filePath := path.Dir(fileName)
+	os.MkdirAll(filePath, 0777)
+	return &LogFile{
+		fileName: fileName,
+		fileMode: "a",
+		filePath: filePath,
+		level:    InfoLevel,
+		prefix:   InfoPrefix,
+		mode:     DayMode,
+		maxSize:  1 << 30, // 1GB
+	}
 }
